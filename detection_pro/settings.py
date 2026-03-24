d1a5d1812dabd52976a729392a61e844f0fb38d5
jishnu-005 <jishnuarullil2005@gmail.com>
2026-03-24 09:04:54 +0530

    Final

--- detection_pro/settings.py
+++ detection_pro/settings.py
@@ -128,8 +128,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER= 'jishnuprakasharullil@gmail.com'
-EMAIL_HOST_PASSWORD= 'mpzw mxkl hqek krxx'
+EMAIL_HOST_USER= 'wildguard97@gmail.com'
+EMAIL_HOST_PASSWORD= 'dknc drdp vgsh qssu'
 
 
 # Default primary key field type
